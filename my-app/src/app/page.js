028d90e7ae6f4c7a2710e9d7dc3ac46b654e61a2
Kumar Priyanshu Raj <kumarpriyanshuraj@csus.edu>
2024-03-28 23:33:27 -0700

    created next.js app

---
+++ my-app/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container text-center">
+      <h1 className="text-4xl font-bold text-black-5000">Welcome to the Realtor Website</h1>
+    </div>
+  );
+}
