4bd1160665bbdbbb742d043022ed79b6a148d91b
MANSOURCSS <Mohamedahmed2@CSUS.EDU>
2024-03-30 12:31:22 -0700

    Change the backround and font color so it's readable :)

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="container text-center">
-      <h1 className="text-4xl font-bold text-black-5000">Welcome to the Realtor Website</h1>
+      <h1 className="text-4xl font-bold text-black">Welcome to the Realtor Website</h1>
     </div>
   );
 }
