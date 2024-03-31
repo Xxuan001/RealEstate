d63572f98c8909cdfda229565175628fa8c8be03
Kumar Priyanshu Raj <kumarpriyanshuraj@csus.edu>
2024-03-30 18:17:05 -0700

    added more options in header bar

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -11,24 +11,34 @@ export default function Home() {
           Real State Services</h1>
       </div>
       <div className="flex items-center bg-gray-800">
-        <button className="ml-6 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
           Home
         </button>
 
-        <button className="ml-6 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-          Buyers
+        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+          Listings
         </button>
 
-        <button className="ml-6 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+          About Me 
+        </button>
+        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+          Sellers
+        </button>
+
+        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+          Buyers
+        </button>
+        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+          Communities
+        </button>
+
+        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
           What's my home worth?
         </button>
 
-        <button className="ml-6 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
           Loan Pre-Approval
-        </button>
-
-        <button className="ml-6 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-          Properties
         </button>
       </div>
     </div>
