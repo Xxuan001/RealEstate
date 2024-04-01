190618d9f93f43cada2cc7a2d56cc6a708b1f8bf
Kumar Priyanshu Raj <kumarpriyanshuraj@csus.edu>
2024-03-31 19:33:59 -0700

    created component called Page

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,46 +1,19 @@
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
+import NavBar from './navbar'; // Make sure the path is correct
 
-export default function Home() {
+function Page() {
   return (
-    // create a navbar on top of the page
     <div className="flex justify-between p-4 bg-gray-800 text-white">
       <div className="flex items-center">
         <Image src="/vercel.svg" alt="logo" width={40} height={40} />
         <h1 className="ml-2 text-3xl font-bold">
-          
-          Real State Services</h1>
+          Real State Services
+        </h1>
       </div>
-      <div className="flex items-center bg-gray-800">
-        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-          Home
-        </button>
-
-        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-          Listings
-        </button>
-
-        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-          About Me 
-        </button>
-        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-          Sellers
-        </button>
-
-        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-          Buyers
-        </button>
-        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-          Communities
-        </button>
-
-        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-          What's my home worth?
-        </button>
-
-        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-          Loan Pre-Approval
-        </button>
-      </div>
+      <NavBar />
     </div>
   );
 }
+
+export default Page;
