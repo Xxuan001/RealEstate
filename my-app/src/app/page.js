6524ecb9919f65ae7fdeb24bf3c32823ab64144b
Kumar Priyanshu Raj <kumarpriyanshuraj@csus.edu>
2024-04-01 00:43:47 -0700

    DP-75 added client logo on headerbar

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -6,10 +6,9 @@ function Page() {
   return (
     <div className="flex justify-between p-4 bg-gray-800 text-white">
       <div className="flex items-center">
-        <Image src="/vercel.svg" alt="logo" width={40} height={40} />
-        <h1 className="ml-2 text-3xl font-bold">
-          Real State Services
-        </h1>
+        <button>
+        <Image src="/logo_.png" alt="logo" width={130} height={130} />
+        </button>
       </div>
       <NavBar />
     </div>
