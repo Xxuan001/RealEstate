10b69f21a4f795b6dd42729ec40f9f7f97b91aa1
Xxuan001 <125781771+Xxuan001@users.noreply.github.com>
2024-04-01 00:50:38 -0700

    logo add and header bg color chaanged
    logo and navigor bg color changed

--- my-app/src/app/navbar.js
+++ my-app/src/app/navbar.js
@@ -1,37 +1,39 @@
 import React from 'react';
 
+
 function NavBar() {
   return (
-    <div className="flex items-center bg-gray-800">
-      <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+    <div className="flex items-center bg-gray-500 justify-start">
+     
+      <button className="ml-2 bg-gray-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
         Home
       </button>
 
-      <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+      <button className="ml-2 bg-gray-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
         Listings
       </button>
 
-      <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+      <button className="ml-2 bg-gray-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
         About Me 
       </button>
 
-      <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+      <button className="ml-2 bg-gray-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
         Sellers
       </button>
 
-      <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+      <button className="ml-2 bg-gray-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
         Buyers
       </button>
 
-        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+        <button className="ml-2 bg-gray-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
             Communities
         </button>
 
-        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+        <button className="ml-2 bg-gray-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
             Properties
         </button>
 
-        <button className="ml-2 bg-gray-800 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+        <button className="ml-2 bg-gray-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
             What's My Home Worth?
         </button>
         
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -4,9 +4,9 @@ import NavBar from './navbar'; // Make sure the path is correct
 
 function Page() {
   return (
-    <div className="flex justify-between p-4 bg-gray-800 text-white">
+    <div className="flex justify-between p-4 bg-gray-500 text-white">
       <div className="flex items-center">
-        <Image src="/vercel.svg" alt="logo" width={40} height={40} />
+        <Image src="/logo-.svg" alt="logo" width={120} height={100} />
         <h1 className="ml-2 text-3xl font-bold">
           Real State Services
         </h1>
